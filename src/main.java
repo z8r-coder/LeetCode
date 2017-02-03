d18c085b4cae23ebc250540aa465d0becee4fba3
RoyRuan <415131215@qq.com>
2017-02-03 16:21:16 +0800

    first commit

---
+++ src/main.java
@@ -0,0 +1,12 @@
+import javax.swing.table.TableStringConverter;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		test();
+	}
+	public  static void test() {
+		System.out.println("hello world");
+	}
+}
