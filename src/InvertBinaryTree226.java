0fb12b8b3ad96c2f2e502b7a389c3c961d0b855d
RoyRuan <415131215@qq.com>
2017-02-04 16:04:22 +0800

    226finish

---
+++ src/InvertBinaryTree226.java
@@ -0,0 +1,23 @@
+
+public class InvertBinaryTree226 {
+    public TreeNode invertTree(TreeNode root) {
+    	if (root == null) {
+			return null;
+		}
+    	if (root.left == null && root.right == null) {
+			return null;
+		}
+    	invertTree(root.left);
+    	invertTree(root.right);
+    	TreeNode temp_node = root.left;
+    	root.left = root.right;
+    	root.right = temp_node;
+        return root;
+    }
+    public class TreeNode {
+    	 int val;
+    	 TreeNode left;
+    	 TreeNode right;
+         TreeNode(int x) { val = x; }
+    }
+}
